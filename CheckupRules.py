54cc6b8ca8c3da0600c055410cc74574355576c3
Michael Braun <michaelbraunbass@gmail.com>
2018-12-19 11:01:03 -0700

    Update CheckupRules.py

--- CheckupRules.py
+++ CheckupRules.py
@@ -124,6 +124,15 @@ rc.send('mgmt_cli -r true set access-rule name "Cleanup rule" layer "APP&URLF" a
 rc.send("\n")
 time.sleep(10)
 
+
+#Install Policy
+rc.send('mgmt_cli -r true publish')
+rc.send("\n")
+time.sleep(10)
+rc.send('mgmt_cli -r true  install-policy policy-package ' + policy_name)
+rc.send("\n")
+time.sleep(460)
+
 #Advanced Settings
 rc.send('''app_obj=$(psql_client cpm postgres -c "select d1.objid, d2.name from dleobjectderef_data d1, dleobjectderef_data d2 where d1.domainid=d2.objid and d1.dlesession=0 and not d1.deleted and d1.name='Application Control & URL Filtering Settings';" | grep SMC | cut -d "|" -f 1)''')
 rc.send("\n")
